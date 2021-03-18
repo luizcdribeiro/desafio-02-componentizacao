6dd76ea4aaa22ee99c720ef7a371e97497907097
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-03-17 22:53:24 -0300

    :tada: Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
+import { useMovies } from "../moviesContext";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+
+  const {movies, selectedGenre} = useMovies();
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+
+  )
+
 }
